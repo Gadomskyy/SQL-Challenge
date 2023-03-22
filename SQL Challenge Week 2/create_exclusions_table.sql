fc8c7a59e37eee04cceeec1d446a4e40aee48ed1
Gadomskyy <j_gadomski@hotmail.com>
2023-03-22 21:41:48 +0100

    add create_exclusions_table.sql
    Part of data cleaning

---
+++ SQL Challenge Week 2/create_exclusions_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE exclusions (
+	pizza_order_id INT,
+    exclusions INT
+    
+    );
+    
+
+INSERT INTO exclusions (pizza_order_id, exclusions)
+VALUES (5,4), (6,4), (7,4), (12,4), (14,2), (14,6);
